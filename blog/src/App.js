639ac8e4a145f99060d99ca3009ff3c5245ba351
HyunYuJin <gusdbwls9772@gmail.com>
2021-10-10 13:07:46 +0900

    props, 스타벅스 컨셉

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,39 +2,67 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  let [title, titleState] = useState(['남자 코트 추천', '강남 우동 맛집', '안녕하세요'])
-  let [ like, likeState ] = useState(0)
+  let [food, foodState] = useState(['Ola Cubano Sandwich', 'Gambas Pasta Mill Box', 'Dolkorom Jjojirong Waffle Sand', 'Barbecue Chicken Cheese Ciabatta', 'Ricotta Cheese Baguette Sandwich.'])
+  let [like, likeState] = useState(0)
 
-  function changeTitle() {
-    let newArray = [...title] // 그냥 copy는 안된다. Deep copy를 할 것(완전 별개의 array)
-    newArray[0] = '여자 코트 추천'
-    titleState(newArray)
-    // titleState(['여자 코트 추천', '강남 우동 맛집', '안녕하세요'])
+  function changeFood() {
+    let newArray = [...food] // 그냥 copy는 안된다. Deep copy를 할 것(완전 별개의 array)
+    newArray[0] = 'Jeju green tea, bacon, cheese bagel'
+    foodState(newArray)
+    // foodState(['Ola Cubano Sandwich', 'Gambas Pasta Mill Box', 'Dolkorom Jjojirong Waffle Sand', 'Barbecue Chicken Cheese Ciabatta', 'Ricotta Cheese Baguette Sandwich.'])
   }
+
+  let [modal, modalState] = useState(false) // 모달창을 on/off
+  let [title, titleState] = useState(0)
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>Starbucks</div>
       </div>
-      <button onClick={ changeTitle }>버튼</button>
-      <div className="list">
-        <h3> { title[0] } <span onClick={ ()=>{ likeState(like + 1) } } >👍</span> { like }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
+
+      <div className="practice-space">
+        <button className="btn" onClick={ changeFood }>Change the first menu.</button>
+        <button className="btn" onClick={ () => modalState(!modal) }>Modal ON/OFF</button>
       </div>
-      <div className="list">
-        <h3> { title[1] } <span onClick={ ()=>{ likeState(like + 1) } } >👍</span> { like }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { title[2] } <span onClick={ ()=>{ likeState(like + 1) } } >👍</span> { like }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      {
+        food.map((write, index) => {
+          return (
+            <div className="list">
+              <h3 className="list-title" onClick={ () => {
+                  titleState(index) 
+                  modalState(!modal)
+              }}>{ write }</h3>
+              <div className="list-info">
+                <p>Published on February { 17 + index }th.</p>
+                <button className="like-btn" onClick={ ()=>{ likeState(like + 1) } } >👍 { like }</button>
+              </div>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal === true
+        ? <Modal propsFood={ food } propsTitle={ title } />
+        : null
+      }
     </div>
   );
 }
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <div className="modal-body">
+        <h2>{ props.propsFood[props.propsTitle] }</h2>
+        <p>날짜</p>
+        <p>내용</p>
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
