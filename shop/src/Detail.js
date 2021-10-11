0f1ebd769a01ed9fdf89cf19c11cca0e1350d312
HyunYuJin <gusdbwls9772@gmail.com>
2021-10-11 20:07:25 +0900

    SCSS, styled-components

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import { useHistory, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import './Detail.scss'
 
 function Detail(props) {
     let history = useHistory()
     let { id } = useParams()
-    let findItem = props.propsShose.find((item) => {
-        return item.id === parseInt(id, 10)
-    })
+    let findItem = props.propsShose.find(item => item.id === parseInt(id, 10))
+
+    let Box = styled.div`
+      padding-top: 20px;
+    `
+    let Title = styled.h3`
+      font-size: 25px;
+      color: ${ props => props.color }
+    `
 
     return (
         <div className="container">
+          <Box>
+            <Title color="red" className="title">Detail</Title>
+          </Box>
+
+          <div className="my-alert-1">
+            <p>재고가 얼마 남지 않았습니다.</p>
+          </div>
+          <div className="my-alert-2">
+            <p>재고가 얼마 남지 않았습니다.</p>
+          </div>
+
           <div className="row">
             <div className="col-md-6">
               <img src={'https://codingapple1.github.io/shop/shoes1.jpg'} alt="shose" width="100%" />
@@ -18,8 +37,8 @@ function Detail(props) {
               <h4 className="pt-5">{ findItem.title }</h4>
               <p>{ findItem.content }</p>
               <p>{ findItem.price }원</p>
-              <button className="btn btn-danger">주문하기</button> 
-              <button className="btn btn-danger" onClick={ () => { history.goBack() }}>뒤로가기</button>
+              <button type="button" className="btn btn-danger">주문하기</button>
+              <button type="button" className="btn btn-warning" onClick={ () => { history.goBack() }}>뒤로가기</button>
             </div>
           </div>
         </div>
