d5e6f1c17a3ffbc6f6bcb8e1fc27c4080f67a6e1
HyunYuJin <gusdbwls9772@gmail.com>
2021-10-11 18:38:17 +0900

    Setting Router

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 import React, { useState } from 'react'
-import './App.css'
 import { Navbar, Nav, Container, Carousel } from 'react-bootstrap'
+import { Route } from 'react-router-dom'
+import './App.css'
 import Data from './data'
 import Card from './Card'
 
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Shop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
@@ -21,44 +22,61 @@ function App() {
         </Container>
       </Navbar>
 
-      <Carousel>
-        <Carousel.Item>
-          <h1 className="d-block w-100 background">First slide</h1>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Route exact path="/">
+        <Carousel>
+          <Carousel.Item>
+            <h1 className="d-block w-100 background">First slide</h1>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <h1 className="d-block w-100 background">Second slide</h1>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <h1 className="d-block w-100 background">Second slide</h1>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <h1 className="d-block w-100 background">Third slide</h1>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+          <Carousel.Item>
+            <h1 className="d-block w-100 background">Third slide</h1>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((item, index) => {
-              return (
-                <Card propsShose={ item } key={ index } />
-              )
-            })
-          }
+        <div className="container">
+          <div className="row">
+            {
+              shoes.map((item, index) => {
+                return (
+                  <Card propsShose={ item } key={ index } />
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
+      </Route>
 
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div>
+      </Route>
     </div>
   )
 }
--- shop/src/index.js
+++ shop/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+import { BrowserRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom' // 좀 더 안전한 라우팅 방법(잘못된 페이지를 서버에 요청해서 404 Page Not Found가 뜰 수 있다. 실수로 서버에게 요청하지 않게 하려면 안전하게 #을 붙여준다.)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
